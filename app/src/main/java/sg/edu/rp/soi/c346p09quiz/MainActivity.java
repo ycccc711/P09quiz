1166e69d79c15380c4f821f1c24e2e3e6816eb8b
Jason Lim <jason_lim@rp.edu.sg>
2019-07-09 11:10:11 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/soi/c346p09quiz/MainActivity.java
@@ -0,0 +1,16 @@
+package sg.edu.rp.soi.c346p09quiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
